4bc2b493bad7209b1154d1d0c3fc0f105d564611
Юрцев Максим Алексеевич <jurtsev.m@gff-rf.ru>
2025-09-04 12:06:33 +0300

    First commit

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import Image from "next/image";
+import "./style.css";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="container">
+                <nav className="header-nav-split">
+                    <div className="header-left">
+                        <Link href="/" className="header-logo">
+                            <Image
+                                src="/logo.svg"
+                                alt="GFF Portal Logo"
+                                width={100}
+                                height={70} />
+                            <div className="header-title-group">
+                                <div className="header-title">Госфильмофонд России</div>
+                                <div className="header-subtitle">Государственный фонд кинофильмов Российской Федерации</div>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="header-center">
+                        <ul className="header-menu">
+                            <li className="menu-item"><Link href="#" className="menu-link active">Главная</Link></li>
+                            <li className="menu-item"><Link href="#" className="menu-link">Документы</Link></li>
+                            <li className="menu-item"><Link href="#" className="menu-link">Архив</Link></li>
+                        </ul>
+                    </div>
+                    <div className="header-right">
+                        <div className="header-search">
+                            <span className="search-icon">
+                                <Image
+                                    src="/search.svg"
+                                    alt="search"
+                                    width={20}
+                                    height={20}
+                                />
+                            </span>
+                            <input type="text" className="search-input" placeholder="Поиск..." />
+                        </div>
+                        <div className="header-user">
+                            <span className="user-icon">
+                                <Image
+                                    src="/user.svg"
+                                    alt="user"
+                                    width={24}
+                                    height={24} />
+                            </span>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const interSans = Inter({
   subsets: ["latin"],
 });
 
@@ -25,8 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${interSans.className} antialiased`}
       >
+        <Header />
         {children}
       </body>
     </html>
